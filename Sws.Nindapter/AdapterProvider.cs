0e9d7b7fdc8e1fb1a972dd3bc34a3f0a3409b341
RedNacho <simon_bluescandal@yahoo.co.uk>
2014-08-23 12:25:22 +0100

    Initial commit (work in progress).

---
+++ Sws.Nindapter/AdapterProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject.Activation;
+
+namespace Sws.Nindapter
+{
+
+    public class AdapterProvider<TAdaptee, T> : Provider<T>
+    {
+
+        private readonly IProvider _adapteeProvider;
+
+        private readonly Func<TAdaptee, T> _adapterFactory;
+
+        public AdapterProvider(IProvider adapteeProvider, Func<TAdaptee, T> adapterFactory)
+        {
+            if (adapteeProvider == null)
+            {
+                throw new ArgumentNullException("adapteeProvider");
+            }
+
+            if (adapterFactory == null)
+            {
+                throw new ArgumentNullException("adapterFactory");
+            }
+
+            _adapteeProvider = adapteeProvider;
+            _adapterFactory = adapterFactory;
+        }
+
+        protected override T CreateInstance(IContext context)
+        {
+            return _adapterFactory((TAdaptee)_adapteeProvider.Create(context));
+        }
+
+    }
+}
