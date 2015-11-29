6723d0aeec79a8eacda1e07a78201fe118a190b2
RedNacho <RedNacho@bitbucket.com>
2015-11-29 18:38:58 +0100

    Added WithDecorator extension method (for use with existing binding).

---
+++ Sws.Nindapter.Tests/BindingWhenInNamedWithOrSyntaxExtensionTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+using Sws.Nindapter.Extensions;
+
+namespace Sws.Nindapter.Tests
+{
+    [TestClass]
+    public class BindingWhenInNamedWithOrSyntaxExtensionTests
+    {
+        [TestMethod]
+        public void WithDecoratorWrapsExistingImplementation()
+        {
+            var kernel = new StandardKernel();
+
+            Func<int, int> addOneFunc = i => i + 1;
+
+            Func<Func<int, int>, Func<int, int>> multiplyByTwoDecoratorFactory
+                = f => i => f(i) * 2;
+
+            kernel.Bind<Func<int, int>>().ToConstant(addOneFunc)
+                .WithDecorator(multiplyByTwoDecoratorFactory);
+
+            var resolved = kernel.Get<Func<int, int>>();
+
+            var result = resolved(4);
+
+            Assert.AreEqual(10, result);
+        }
+    }
+}
