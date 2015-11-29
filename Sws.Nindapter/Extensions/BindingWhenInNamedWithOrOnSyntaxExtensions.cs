f58f53671bc9e0327b9071811bcafe6a9850010b
RedNachoBitbucket <rednachobitbucket@example.com>
2015-11-29 18:38:58 +0100

    Added WithDecorator extension method (for use with existing binding).

---
+++ Sws.Nindapter/Extensions/BindingWhenInNamedWithOrOnSyntaxExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Ninject.Infrastructure.Introspection;
+using Ninject.Planning.Bindings;
+using Ninject.Syntax;
+
+namespace Sws.Nindapter.Extensions
+{
+    public static class BindingWhenInNamedWithOrOnSyntaxExtensions
+    {
+        public static IBindingWhenInNamedWithOrOnSyntax<T> WithDecorator<T>(this IBindingWhenInNamedWithOrOnSyntax<T> bindingWhenInNamedWithOrOnSyntax, Func<T, T> decoratorFactory)
+        {
+            var bindingConfiguration = bindingWhenInNamedWithOrOnSyntax.BindingConfiguration;
+
+            var providerCallback = bindingConfiguration.ProviderCallback;
+
+            var adapterProviderFactory = new AdapterProviderFactory();
+
+            bindingConfiguration.ProviderCallback = context =>
+                adapterProviderFactory.CreateAdapterProvider(providerCallback(context), decoratorFactory);
+
+            return new BindingConfigurationBuilder<T>(bindingConfiguration, typeof(T).Format(), bindingWhenInNamedWithOrOnSyntax.Kernel);
+        }
+    }
+}
